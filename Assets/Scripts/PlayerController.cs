2a415afc156fc6a30ac8e5e55419353358df9d93
Kazi Jawadul Islam Jishan <kazijawadulislam123@gmail.com>
2023-07-08 03:14:51 +0600

    basic movement and camera follows player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float verticalInput;
     public float speed;
+    public bool facingRight;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +23,25 @@ public class PlayerController : MonoBehaviour
 
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.up * speed * Time.deltaTime * verticalInput);
+
+        if(horizontalInput > 0 && !facingRight) 
+        {
+            //gameObject.transform.localScale = new Vector3(1, 1, 1);
+            Flip();
+        }
+        else if (horizontalInput < 0 && facingRight)
+        {
+            //gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 }
